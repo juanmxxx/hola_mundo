411151cd3d05bd1777bac5b2aa242e08dc193447
Juan Ma <juanmanuelsujargonzalez@gmail.com>
2023-10-09 09:04:20 +0200

    Cambio de la app

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the pito this many times:',
+              'You have pushed the pito ths many times:',
             ),
             Text(
               '$_counter',
